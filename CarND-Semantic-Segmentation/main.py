dbc21c06f0f70a1bd031ba0e5b1977de23ef022b
Yue Jiang <maze1024@hotmail.com>
2018-05-17 13:05:04 +0800

    finish CarND-Semantic-Segmentation

--- CarND-Semantic-Segmentation/main.py
+++ CarND-Semantic-Segmentation/main.py
@@ -80,7 +80,6 @@ def optimize(nn_last_layer, correct_label, learning_rate, num_classes):
     logits = tf.reshape(nn_last_layer,(-1,num_classes))
     y = tf.reshape(correct_label, (-1, num_classes))
 
-    # now define a loss function and a trainer/optimizer
     cross_entropy_loss = tf.reduce_mean(
         tf.nn.softmax_cross_entropy_with_logits(logits=logits, labels=y))
     train_op = tf.train.AdamOptimizer(
